63aa0183b57d9305df6d706f918cef5e9336acaa
GabyDevX <gabo.ramirez0811@gmail.com>
2022-09-04 13:14:53 -0300

    Currency gallery completed and inserted into the html lading page, I've started responsive, so as of now the  navbar, hero and one more section are already responsive

--- js/main.js
+++ js/main.js
@@ -1,29 +1,47 @@
-// const carousel = document.getElementById('carousel')
-// let currencies
-// let property
+const carousel = document.getElementById('carousel')
+let currencies
+let dinamicData = []
 
-// const currencyTemplate = (description, iso, rate) => {
-//   const container = document.createElement('div')
-//   container.className = 'carousel-item active w-100'
-//   container.innerHTML = `
-//     <p class="bitcoin__description" >${description}</p>
-//               <p class="bitcoin__iso" >${iso}</p>
-//               <p class="bitcoin__rate" >${rate}</p>`
-//   return container
-// }
+const currencyTemplate = (description, iso, rate) => {
+  const container = document.createElement('div')
+  container.className = 'carousel-item'
+  container.innerHTML = `
+    <img src="assets/bitcoinBG.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption">
+              <h5 class="bitcoin__description" id="bitcoin__description">
+                ${description}
+              </h5>
+              <div>
+                <span>ISO Code:</span>
+                <span class="bitcoin__iso" id="bitcoin__iso">${iso}</span>
+              </div>
+              <div>
+                <span>Rate:</span>
+                <span class="bitcoin__rate" id="bitcoin__rate">${rate}</span>
+              </div>
+            </div>`
+  return container
+}
 
-// const getData = () => {
-//   fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-//     .then((response) => response.json())
-//     .then((data) => {
-//       currencies = data.bpi
-//       for (const property in currencies) {
-//         currency = currencies[property]
-//         carousel.appendChild(
-//           currencyTemplate(currency.description, currency.code, currency.rate),
-//         )
-//       }
-//     })
-// }
-// carousel.innerHTML = ''
-// getData()
+const getData = () => {
+  fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then((response) => response.json())
+    .then((data) => {
+      currencies = data.bpi
+      for (const property in currencies) {
+        currency = currencies[property]
+        dinamicData.push(
+          currencyTemplate(currency.description, currency.code, currency.rate),
+        )
+      }
+      dinamicData[0].classList.add('active')
+      dinamicData.forEach((currency) => carousel.appendChild(currency))
+    })
+    .catch(() => {
+      let errorItem = currencyTemplate('No data', 'No data', 'No data')
+      errorItem.classList.add('active')
+      carousel.appendChild(errorItem)
+    })
+}
+
+getData()
